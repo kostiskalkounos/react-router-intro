8dda58035993fed74d24ae24b6c48d9142e3c884
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-18 03:29:02 +0200

    add dynamic routes with params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A book</li>
+        <li>A carpet</li>
+        <li>A skateboard</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
